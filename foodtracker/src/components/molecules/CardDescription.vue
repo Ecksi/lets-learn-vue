af94e5e1edbf8337db50567f0b071dadee62c32a
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-20 09:18:46 -0700

    Add CardDescription component

---
+++ foodtracker/src/components/molecules/CardDescription.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    CardDescription..
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CardDescription',
+  }
+</script>
+
+<style scoped>
+
+</style>
--- foodtracker/src/components/organisms/FoodCard.vue
+++ foodtracker/src/components/organisms/FoodCard.vue
@@ -3,18 +3,21 @@
     <CardHeader />
     <VoteCount voteType="👍" />
     <VoteCount voteType="👎" />
+    <CardDescription />
   </div>
 </template>
 
 <script>
   import VoteCount from '../atoms/VoteCount';
   import CardHeader from '../molecules/CardHeader';
+  import CardDescription from '../molecules/CardDescription';
 
   export default {
     name: 'FoodCard',
     components: {
       VoteCount,
       CardHeader,
+      CardDescription,
     },
   }
 </script>
