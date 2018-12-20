825323fa7b0a8c968863da606f9b5a7c735db6d9
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-20 08:43:12 -0700

    Add voteCount component

--- foodtracker/src/App.vue
+++ foodtracker/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <VoteCount voteType="👍" /> <!-- can i do type === '' ? 'someDefault' : validType -->
+    <VoteCount voteType="👍" />
     <VoteCount voteType="👎" />
   </div>
 </template>
 
 <script>
-import VoteCount from './components/VoteCount.vue'
+import VoteCount from './components/atoms/VoteCount.vue'
 
 export default {
   name: 'app',
@@ -17,13 +17,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    text-align: center;
+  }
 </style>
---
+++ foodtracker/src/components/atoms/VoteCount.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="vote">
+    <span>{{ count }}</span>
+    <span v-on:click="count++">{{ voteType }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      voteType: {
+        type: String,
+        required: true,
+      }
+    },
+    name: 'VoteCount',
+    data: () => {
+      return {
+        count: 0,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
