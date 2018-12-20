8be8bad9f7d4365185eee259bed3e98b03e0177e
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-20 08:26:07 -0700

    Remove unused components and code

---
+++ foodtracker/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <VoteCount voteType="👍" /> <!-- can i do type === '' ? 'someDefault' : validType -->
+    <VoteCount voteType="👎" />
+  </div>
+</template>
+
+<script>
+import VoteCount from './components/VoteCount.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VoteCount
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
