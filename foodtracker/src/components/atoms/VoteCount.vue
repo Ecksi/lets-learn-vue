b247c65525c0a27e4990c717ee28b809cbd7eb33
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-25 06:02:51 -0700

    Pass click event info to parent

--- foodtracker/src/components/atoms/VoteCount.vue
+++ foodtracker/src/components/atoms/VoteCount.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="vote">
     <span>{{ count }}</span>
-    <span v-on:click="count++">{{ voteType }}</span>
+    <span v-on:click="addVote(voteType)">{{ voteType === 'up' ? "👍" : "👎" }}</span>
   </div>
 </template>
 
 <script>
   export default {
     props: {
+      count: {
+        type: Number,
+        required: true
+      },
       voteType: {
         type: String,
         required: true,
       }
     },
     name: 'VoteCount',
-    data: () => {
-      return {
-        count: 0,
+    methods: {
+      addVote(voteType) {
+        this.$emit('addVote', voteType);
       }
-    }
+    },
   }
 </script>
 
 <style scoped>
+span {
+  padding: 0 4px;
+}
 </style>
