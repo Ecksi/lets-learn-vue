50362a8e1ec53b7b170e1dd2a5b9c3dd2f2219ff
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-25 06:05:01 -0700

    Add fetch get foods, put vote calls

--- foodtracker/src/App.vue
+++ foodtracker/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <AppHeader />
-    <FoodCard />
+    <div v-for="food in foods">
+      <FoodCard :food="food" v-on:addVote="moreVote" />
+    </div>
   </div>
 </template>
 
@@ -13,7 +15,33 @@
     name: 'app',
     components: {
       AppHeader,
-      FoodCard,
+      FoodCard
+    },
+    computed: {
+      fetchFoods() {
+        fetch('https://cors-anywhere.herokuapp.com/http://calm-caverns-24814.herokuapp.com/food').then(res => res.json()).then(data => this.foods = data)
+      },
+    },
+    data: () => {
+      return {
+        foods: [],
+      }
+    },
+    methods: {
+      moreVote(type, id) {
+        fetch(`https://cors-anywhere.herokuapp.com/http://calm-caverns-24814.herokuapp.com/food/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({vote: type}),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.json())
+        .then(response => {
+          console.log('Success:', JSON.stringify(response));
+          this.foods.forEach(el => el.id === id ? el[type+'_vote'] += 1 : '');
+        })
+        .catch(error => console.error('Error:', error));
+      }
     }
   }
 </script>
@@ -21,5 +49,9 @@
 <style scoped>
   #app {
     text-align: center;
+  }
+
+  .food-card-container {
+    border-top: 1px solid #999;
   }
 </style>
