55370dcd3cfcc378a2d5dd3ca2f9eb816e6c1720
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-26 08:57:50 -0700

    Add Modal component to AppHeader

--- foodtracker/src/components/molecules/AppHeader.vue
+++ foodtracker/src/components/molecules/AppHeader.vue
@@ -1,21 +1,99 @@
 <template>
   <div class="header">
-    <div>Add Food</div>
+    <div class="modal-container">
+      <button type="button" class="add-food" @click="showModal">Add Food</button>
+      <Modal v-on:addFood="addFood" v-show="isModalVisible" @close="closeModal" />
+    </div>
     <h1>Free 9er Food Feedback</h1>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'AppHeader',
-  }
+import Modal from '../atoms/Modal';
+
+export default {
+  name: 'AppHeader',
+  components: {
+    Modal,
+  },
+  data () {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+    addFood(name, desc, image) {
+      this.$emit('addFood', name, desc, image);
+    }
+  },
+}
 </script>
 
 <style scoped>
-  .header {
-    display: flex;
-    justify-content: space-between;
-    width: 960px;
-    margin: 0 auto;
-  }
+.header {
+  display: flex;
+  justify-content: space-between;
+  width: 660px;
+  margin: 0 auto;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto; 
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.add-food {
+  border: none;
+  cursor: pointer;
+  width: 125px;
+  font-size: 18px;
+  font-weight: lighter;
+  outline: none;
+}
+
+.modal-container {
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+h1 {
+  text-align: right;
+  width: 100%;
+}
 </style>
