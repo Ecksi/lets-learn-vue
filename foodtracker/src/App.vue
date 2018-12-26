78aeaa627c3ded10514543979d99678b9586b30d
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-26 08:56:28 -0700

    Add post Food fetch, move initial fetch to created method

--- foodtracker/src/App.vue
+++ foodtracker/src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    <AppHeader />
-    <div v-for="food in foods">
-      <FoodCard :food="food" v-on:addVote="moreVote" />
+    <AppHeader v-on:addFood="addFood" />
+    <div v-for="food in foods" :key="food.id">
+      <FoodCard :food="food" v-on:addVote="addVote" />
     </div>
   </div>
+  <!-- limit search to 3 per page
+
+      add search filters 
+        most upvotes
+        most downvotes
+        most comments
+        newest
+        name
+   -->
 </template>
 
 <script>
@@ -17,18 +26,19 @@
       AppHeader,
       FoodCard
     },
-    computed: {
-      fetchFoods() {
-        fetch('https://cors-anywhere.herokuapp.com/http://calm-caverns-24814.herokuapp.com/food').then(res => res.json()).then(data => this.foods = data)
-      },
-    },
     data: () => {
       return {
         foods: [],
       }
     },
+    created() {
+      fetch('https://cors-anywhere.herokuapp.com/http://calm-caverns-24814.herokuapp.com/food')
+        .then(res => res.json())
+        .then(data => this.foods = data)
+        .catch(error => console.error('Error', error))
+    },
     methods: {
-      moreVote(type, id) {
+      addVote(type, id) {
         fetch(`https://cors-anywhere.herokuapp.com/http://calm-caverns-24814.herokuapp.com/food/${id}`, {
           method: 'PUT',
           body: JSON.stringify({vote: type}),
@@ -39,6 +49,25 @@
         .then(response => {
           console.log('Success:', JSON.stringify(response));
           this.foods.forEach(el => el.id === id ? el[type+'_vote'] += 1 : '');
+        })
+        .catch(error => console.error('Error:', error));
+      },
+      addFood(name, desc, image) {
+        fetch(`https://cors-anywhere.herokuapp.com/http://calm-caverns-24814.herokuapp.com/food/`, {
+          method: 'POST',
+          body: JSON.stringify({
+            'name': name,
+            'description': desc,
+            'image_url': image,
+          }),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.json())
+        .then(response => {
+          console.log('Success:', JSON.stringify(response));
+          this.foods.push(response);
+          window.alert(`You just added ${response.name} to the 9er Food Review`);
         })
         .catch(error => console.error('Error:', error));
       }
