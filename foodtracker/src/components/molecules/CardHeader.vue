18dbbeed213e8f8dc8333d1f33ad571ddd7f67d5
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-26 20:20:11 -0700

    Add CardHeader component

---
+++ foodtracker/src/components/molecules/CardHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card-header">
+    <slot />
+    <DateSanitizer :date="date" />
+  </div>
+</template>
+
+<script>
+  import DateSanitizer from '../atoms/DateSanitizer';
+
+  export default {
+    name: 'CardHeader',
+    components: {
+      DateSanitizer,
+    },
+    props: {
+      date: {
+        type: String,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 15px 0 15px;
+}
+</style>
