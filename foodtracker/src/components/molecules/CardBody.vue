672d65164f1bc6055fec5b37bfefc22c626f0c7e
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-26 20:41:36 -0700

    Add CardBody component

---
+++ foodtracker/src/components/molecules/CardBody.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card-body">
+    <VoteMachine @addVote="addVote" :upvote="upvote" :downvote="downvote" />
+    <span class="card-description"><slot /></span>
+  </div>
+</template>
+
+<script>
+  import VoteMachine from '../organisms/VoteMachine';
+
+  export default {
+    name: 'CardBody',
+    components: {
+      VoteMachine,
+    },
+    props: {
+      upvote: Number,
+      downvote: Number,
+    },
+    methods: {
+      addVote(voteType) {
+        this.$emit('addVote', voteType);
+      },
+    }
+  }
+</script>
+
+<style>
+.card-body {
+  display: flex;
+  justify-content: space-between;
+}
+
+.card-description {
+  width: 100%;
+  text-align: left;
+  padding-left: 5px;
+  padding-top: 20px;
+}
+</style>
