ee97cbb0fc68eb47770784da7c03a74ef2ce63e3
Jim Dechant <James.dechant@homeadvisor.com>
2018-12-26 20:56:57 -0700

    Add Delete component

---
+++ foodtracker/src/components/atoms/DeleteThing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @click="deleteThing" class="delete-thing">
+    &times;
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'DeleteThing',
+    methods: {
+      deleteThing() {
+        this.$emit('delete');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.delete-thing {
+  cursor: pointer;
+}
+</style>
