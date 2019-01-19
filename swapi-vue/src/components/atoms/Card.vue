af049b7de682a315f85bfb0a9bc384713b827035
Jim Dechant <James.dechant@homeadvisor.com>
2019-01-19 11:31:59 -0700

    Add Card component

---
+++ swapi-vue/src/components/atoms/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='card'>
+    I am a card
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card'
+  }
+</script>
+
+<style scoped lang='less'>
+  .card {
+    border: 2px solid black;
+    height: 225px;
+    margin-bottom: 30px;
+  }
+</style>
