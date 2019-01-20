c747ad7f3b24be021746be8e8db14260734e5a63
Jim Dechant <James.dechant@homeadvisor.com>
2019-01-20 15:12:40 -0700

    Add Pagination component

---
+++ swapi-vue/src/components/atoms/Pagination.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='pagination'>
+    <span v-show='prev' @click='$emit("prevSet")' class='paginate'>Prev </span>
+    <ul v-for='index in pageCount' :key='index'>
+      <li @click='$emit("jumpTo", index)'>{{index}}</li>
+    </ul>
+    <span v-show='next' @click='$emit("nextSet")' class='paginate'> Next</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Pagination',
+    props: {
+      prev: String,
+      next: String,
+      count: Number
+    },
+    computed: {
+      pageCount() {
+        return Math.ceil(this.count / 10)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .pagination {
+    text-align: center;
+  }
+  .paginate {
+    cursor: pointer;
+  }
+  ul {
+    display: inline;
+    padding: 0;
+    margin: 0 5px;
+  }
+  li {
+    display: inline;
+    list-style: none;
+  }
+</style>
