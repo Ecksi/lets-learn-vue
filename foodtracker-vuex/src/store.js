e8a481a673bcb581644b6b88787ca292a24139d1
Jim Dechant <James.dechant@homeadvisor.com>
2019-01-27 16:57:49 -0700

    Add initial store state, methods

--- foodtracker-vuex/src/store.js
+++ foodtracker-vuex/src/store.js
@@ -1,16 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { getAllFoods } from './js/fetchCalls.js';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    foods: []
   },
   mutations: {
-
+    GET_ALL_FOODS: (state, payload) => state.foods = payload
   },
   actions: {
-
+    getFoods: ({commit}) => getAllFoods()
+      .then(({data}) => commit('GET_ALL_FOODS', data))
+  },
+  getters: {
+    countLinks: state => state.foods.length
   }
 })
--- foodtracker-vuex/src/views/Home.vue
+++ foodtracker-vuex/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <FoodTrackerLanding />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  import FoodTrackerLanding from '@/components/FoodTrackerLanding';
+  import VoteMachine from '@/components/molecules/VoteMachine';
+  export default {
+    name: 'home',
+    components: {
+      FoodTrackerLanding,
+      VoteMachine
+    }
   }
-}
 </script>
